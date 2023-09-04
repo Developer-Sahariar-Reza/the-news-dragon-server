568ef6b93def55b2a4721e525366035b1febf71a
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-09-04 13:43:24 +0600

    categories.json done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+const categories = require("./data/categories.json");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("The Dragon News Server");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log(`The Dragon News Server Running in: ${port}`);
+});
